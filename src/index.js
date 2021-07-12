db8c93e443c2be05e5bb709a5a892708ae96783d
Jodu555 <Jodu505@gmail.com>
2021-07-12 16:15:08 +0200

    Added Change and Persistent Data

--- src/index.js
+++ src/index.js
@@ -2,30 +2,45 @@ const machineInfo = require('./machineInfo');
 const os = require('os');
 const io = require("socket.io-client");
 
+const PERSISTENT_DATA = 'PERSISTENT_DATA';
+const CHANGE_DATA = 'CHANGE_DATA';
+
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
     console.log('Connected');
     socket.on('action', (msg) => {
         console.log('ACTION:', msg);
-        if (msg == 'PERSISTENT_DATA') {
-            socket.emit('data', {
-                type: msg,
-                ips: machineInfo.getRegisteredIPs(),
-                host: machineInfo.getHostInfo(),
-            });
+        if (msg == PERSISTENT_DATA) {
+            socket.emit('data', getPersistentData());
+        } else if (msg == CHANGE_DATA) {
+            socket.emit('data', getChangeData());
         }
-
     })
 });
 
+function getPersistentData() {
+    return {
+        type: PERSISTENT_DATA,
+        ips: machineInfo.getRegisteredIPs(),
+        host: machineInfo.getHostInfo(),
+    };
+}
 
+async function getChangeData() {
+    return {
+        type: CHANGE_DATA,
+        loadavg: os.loadavg(),
+        uptime: machineInfo.getUptimeInSeconds(),
+        memory: machineInfo.getMemoryInfo(),
+        cpu: await machineInfo.getCPU(),
+    };
+}
 
 // console.log('IPS: ', machineInfo.getRegisteredIPs());
 // console.log('HOST: ', machineInfo.getHostInfo());
 // console.log('LOAD-AVG: ', os.loadavg());
 // console.log('UPTIME: ', machineInfo.getUptimeInSeconds());
-
 // console.log('memory: ', machineInfo.getMemoryInfo());
 // (async () => {
 //     console.log('CPU: ', await machineInfo.getCPU());
