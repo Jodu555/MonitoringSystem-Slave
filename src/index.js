a8edf0bb93f820cc6fd23df76f297eebd0ff2613
Jodu555 <Jodu505@gmail.com>
2021-09-13 14:54:53 +0200

    Another dumb mistake

--- src/index.js
+++ src/index.js
@@ -19,18 +19,12 @@ const PERSISTENT_DATA = 'PERSISTENT_DATA';
 const CHANGE_DATA = 'CHANGE_DATA';
 const socket = io(config.coreIP);
 
-
-socket.on('connect', () => {
-    console.log('Connected');
-});
 socket.on('disconnect', () => {
     console.log('Disconnected');
 });
-socket.emit('type', { type: 'slave' })
 socket.on('auth', (data) => {
     console.log('Authentication:', data ? 'Success!' : 'Failed');
 });
-socket.emit('auth', { auth_token: config.key });
 socket.on('action', async (msg) => {
     console.log('ACTION:', msg);
     if (msg == PERSISTENT_DATA) {
@@ -39,6 +33,12 @@ socket.on('action', async (msg) => {
         const data = await getChangeData()
         socket.emit('data', data);
     }
+});
+
+socket.on('connect', () => {
+    console.log('Connected');
+    socket.emit('type', { type: 'slave' })
+    socket.emit('auth', { auth_token: config.key });
 });
 
 function getPersistentData() {
