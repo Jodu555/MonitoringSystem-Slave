23bfe54782021b82cd36a43c18a819719de4167d
Jodu555 <Jodu505@gmail.com>
2021-09-30 00:52:27 +0200

    Added the set command

--- src/index.js
+++ src/index.js
@@ -22,7 +22,23 @@ const SocketManager = new socketManager(config, CommandManager);
 CommandManager.registerCommand(new Command('uptime', 'uptime', 'Displays the second time the system is running!', () => {
     const seconds = Math.round((Date.now() - startup) / 1000);
     console.log('The system is running since ' + seconds + ' second' + (seconds > 1 ? 's' : '') + '!');
-}))
+}));
+
+CommandManager.registerCommand(new Command('set', 'set [authkey/key, coreip/ip]', 'Sets the Config value\'s!', (cmd, args) => {
+    const key = args[1].toLowerCase();
+    const value = args[2];
+    if (key == 'authkey' || key == 'key') {
+        config.key = value;
+        console.log('Auth Key Set to: ' + value);
+    }
+    if (key == 'coreip' || key == 'ip') {
+        config.coreIP = value;
+        console.log('Core IP Set to: ' + value);
+    }
+
+    fs.writeFileSync('config.json', JSON.stringify(config, null, 3));
+    console.log('The key dont exists!');
+}));
 
 
 
