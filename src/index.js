a81544c1d9c164ca420929dbb486e8d44d62652b
Jodu555 <Jodu505@gmail.com>
2021-09-29 20:39:06 +0200

    Fix: other calc

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const socketManager = require('./socketManager');
 const SocketManager = new socketManager(config, CommandManager);
 
 CommandManager.registerCommand(new Command('uptime', 'uptime', 'Displays the second time the system is running!', () => {
-    console.log(Date.now() - startup * 1000);
+    console.log(Date.now() - startup / 1000);
 }))
 
 
