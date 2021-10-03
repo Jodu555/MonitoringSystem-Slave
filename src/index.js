cf35705526a83f5393cd4f699a040f6be2228ee3
Jodu555 <Jodu505@gmail.com>
2021-10-03 19:39:38 +0200

    Moved the config stuff in its own fnction

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ const { commandManager, Command } = require('./commandManager');
 
 //Config Stuff
 let config;
-if (fs.existsSync('config.json')) {
-    config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
-} else {
-    config = {
-        coreIP: 'http://localhost:3000',
-        key: 'INSERT-YOUR-KEY-HERE'
-    };
-    fs.writeFileSync('config.json', JSON.stringify(config, null, 3));
+manageConfig();
+function manageConfig() {
+    if (fs.existsSync('config.json')) {
+        config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+    } else {
+        config = {
+            coreIP: 'http://localhost:3000',
+            key: 'INSERT-YOUR-KEY-HERE'
+        };
+        fs.writeFileSync('config.json', JSON.stringify(config, null, 3));
+    }
 }
+
 
 const CommandManager = new commandManager(process.stdin, process.stdout);
 
